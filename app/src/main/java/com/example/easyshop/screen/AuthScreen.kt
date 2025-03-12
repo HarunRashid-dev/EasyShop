06499c2ee9dfc0719cc0f253af58d922162a7049
HarunRashid-dev <harunrashid9159@gmail.com>
2025-03-12 11:16:09 +0530

    7:36

--- app/src/main/java/com/example/easyshop/screen/AuthScreen.kt
+++ app/src/main/java/com/example/easyshop/screen/AuthScreen.kt
@@ -1,26 +1,79 @@
 package com.example.easyshop.screen
 
+import android.R.attr.onClick
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.R
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.easyshop.R
 
 @Composable
 fun AuthScreen(modifier: Modifier = Modifier){
     Column (
         modifier = modifier
             .fillMaxSize()
-            .padding(32.dp)
+            .padding(32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
     ){
         Image(
             painter = painterResource(id = R.drawable.banner),
-            contentDescription = "Banner"
+            contentDescription = "Banner",
+            modifier = Modifier.fillMaxWidth()
+                .height(300.dp)
         )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(text = "Start your shopping journey now",
+        style = TextStyle(
+            fontSize = 30.sp,
+            fontFamily = FontFamily.Monospace,
+            fontWeight = FontWeight.SemiBold,
+            textAlign = TextAlign.Center
+            )
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(text = "Best ecom platform with best prices",
+            style = TextStyle(
+                textAlign = TextAlign.Center
+            )
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(onClick = { },
+            modifier= Modifier.fillMaxWidth()
+                .height(60.dp)
+            ){
+            Text(text = "Login", fontSize = 22.sp)
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(onClick = { },
+            modifier= Modifier.fillMaxWidth()
+                .height(60.dp)
+        ){
+            Text(text = "Signup", fontSize = 22.sp)
+        }
+
     }
 }
