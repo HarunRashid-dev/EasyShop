bb01075c4028394614e4dd138a0ad1ed8e4ab73a
HarunRashid-dev <harunrashid9159@gmail.com>
2025-03-13 09:39:33 +0530

    https://youtu.be/uKcBJoUIIaM?si=9qWrq8-b_zhjPQVQ
    1:58

--- app/src/main/java/com/example/easyshop/screen/SignupScreen.kt
+++ app/src/main/java/com/example/easyshop/screen/SignupScreen.kt
@@ -1,10 +1,50 @@
 package com.example.easyshop.screen
 
+import android.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SignupScreen (modifier: Modifier = Modifier){
-    Text(text="Signup")
+
+    Column(
+        modifier = modifier.fillMaxSize()
+            .padding(32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(text = "Hello there!",
+            modifier = Modifier.fillMaxWidth(),
+            style = TextStyle(
+                fontSize = 30.sp,
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.Bold,
+            )
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(
+            text = "Create an account",
+            modifier = Modifier.fillMaxWidth(),
+            style = TextStyle(
+                fontSize = 22.sp,
+            )
+        )
+
+    }
 }
