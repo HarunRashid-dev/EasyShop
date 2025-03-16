57a68eb8ada83098b7e131827bbc9c06e0a05586
HarunRashid-dev <harunrashid9159@gmail.com>
2025-03-16 10:47:08 +0530

    VD 3 Completed till 6:09

---
+++ app/src/main/java/com/example/easyshop/viewmodel/AuthViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.easyshop.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.firestore
+
+class AuthViewModel : ViewModel() {
+
+    private val auth = Firebase.auth
+
+    private val firestore = Firebase.firestore
+
+    fun login(){
+
+    }
+
+    fun signup(email : String, name : String, password : String, onResult: (Boolean, String?)-> Unit){
+        auth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener {
+                if(it.isSuccessful){
+
+                }else{
+                    onResult(false,it.exception?.localizedMessage)
+                }
+            }
+    }
+
+}
